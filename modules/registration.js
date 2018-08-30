48757842903f94a6f40fbcd85c98aab8119b1bdb
Artyom Sheldyaev <cmashinho@gmail.com>
2018-08-30 20:39:04 +0300

    Rework

--- modules/registration.js
+++ modules/registration.js
@@ -5,19 +5,19 @@ const utils = require('./utils')
 exports.registration = function(req, res) {
     utils.readBody(req, res).then(
         result => {
-            db.users.addNewUser(req.body.login, req.body.password).then(
+            db.users.addNewUser(result.login, result.password).then(
                 result => {
                     send_answer('templates/index.html', res, 'text/html')
                 },
                 error => {
-                    // Add error check
+                    console.log(error)
                     res.statusCode = 400
                     res.end("Something went wrong")
                 }
             )
         },
         error => {
-            
+
         }
     )
 }
