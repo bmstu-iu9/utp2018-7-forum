c10c0a3ff51c976fbb49acdf112813ed7073d16c
Artyom Sheldyaev <cmashinho@gmail.com>
2018-08-30 20:09:23 +0300

    Added method to read request body

---
+++ modules/utils.js
@@ -0,0 +1,30 @@
+exports.readBody = function(req, res) {
+    return new Promise(function(resolve, reject) {
+        let body = ''
+
+        req.on('readable', function() {
+            let data = req.read()
+            if (data != null) {
+                body += data
+            }
+            if (body.length > 1e6) {
+                console.log('Body too long')
+                res.statusCode = 413;
+                res.end('Body too long')
+                reject('413: Body too long')
+            }
+        })
+        .on('end', function() {
+            try {
+                if (body) {
+                    body = JSON.parse(body)
+                }
+                resolve(body)
+            } catch (err) {
+                res.statusCode = 400;
+                res.end("Can't parse body")
+                reject("400: Can't parse body")
+            }
+        })
+    })
+}
