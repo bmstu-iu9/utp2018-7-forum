584c3dbaa7807139d70dd8098dca851a997bc319
Artyom Sheldyaev <cmashinho@gmail.com>
2018-09-02 15:29:13 +0300

    Added cookies to registration module

--- modules/registration.js
+++ modules/registration.js
@@ -8,8 +8,17 @@ exports.registration = function(req, res) {
             console.log(result.login, result.password)
             db.users.addUser(result.login, result.password).then(
                 result => {
-                    console.log('success')
-                    send_answer('templates/index.html', res, 'text/html')
+                    db.sessions.createSession(result.login).then(
+                        function(data) {
+                            let id = 'session_id=' + data + '; Path=/; Secure;'
+                            let login = 'login=' + result.login
+                            send_answer('templates/index.html', res, 'text/html', cookies=[id, login])
+                        }.catch(function(err) {
+                            console.log(err)
+                            res.statusCode = 400
+                            res.end('Something went wrong')
+                        })
+                    )
                 },
                 error => {
                     console.log(error)
