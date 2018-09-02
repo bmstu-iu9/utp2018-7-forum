85f65cd315977036ccba025afdc2cca7f0c6ab4f
Artyom Sheldyaev <cmashinho@gmail.com>
2018-09-02 15:27:33 +0300

    Rework send method for cookies

--- modules/send.js
+++ modules/send.js
@@ -1,10 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function(file, res, content) {
+module.exports = function(file, res, content, cookies=null) {
     if (fs.existsSync(path.resolve('', file))) {
         html = fs.readFileSync(file)
-        res.writeHead(200, {'Content-Type': content})
+        to_head = {
+            'Content-Type': content
+        }
+        if cookies {
+            to_head['Set-Cookie'] = cookies
+        }
+        res.writeHead(200, to_head)
         res.end(html)
     } else {
         res.writeHead(404, {'Content-Type': content})
