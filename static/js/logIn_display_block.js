356e5393826d583c4a45ec597837f307d23187c3
Ivan <ivan.filonenko@gmail.com>
2018-09-03 19:55:23 +0300

    Changed behavior of logIn window
    All styles of html files moved into one css file

--- static/js/logIn_display_block.js
+++ static/js/logIn_display_block.js
@@ -1,6 +1,9 @@
 if (document.cookie.includes('session_id')) {
 	document.getElementById('logedIn').style.display = "block";
 }
+if (!document.cookie.includes('session_id')) {
+	document.getElementById('logIn').style.display = "block";
+}
 var a = document.cookie.split(';');
 for(var i=0; i<a.length; i++){
 	if(a[i].indexOf("login")>-1)
--- static/js/logIn_display_none.js
+++
@@ -1,3 +0,0 @@
-if (document.cookie.includes('session_id')) {
-	document.getElementById('logIn').style.display = "none";
-}
